88be6536fd039bd23ddc11aa6084bc5db3721130
Grubbybio <grubbybio@gmail.com>
2021-07-15 21:14:07 +0800

    sup

--- scripts/RNAmodR.R
+++ scripts/RNAmodR.R
@@ -11,12 +11,12 @@ sequences <- paste(args[1], "fa", sep=".")
 annotation <- GFF3File(paste(args[1], "gff", sep="."))
 file <- c(treated = paste(args[1],"filtered","bam",sep="."))
 mrms <- ModRiboMethSeq(file, annotation = annotation, sequences = sequences)
-settings(mrms) <- list(minScoreMean = 0.93, minScoreRMS = 0.5, minCoverage = as.integer(args[2]))
+settings(mrms) <- list(minScoreMean = 0.93, minScoreA = 0.5, minCoverage = as.integer(args[2]))
 modify(mrms,force = TRUE) -> mrms
-sink(paste(args[1],"RMS","wig",sep="."))
+sink(paste(args[1],"ScoreA","wig",sep="."))
 for(i in 1:length(ranges(mrms))){
   cat(paste("variableStep chrom=",as.character(seqnames(ranges(mrms)))[i],"\n",sep=""))
-  values(getDataTrack(mrms,i,type = c("scoreRMS", "scoreMean"))$scoreRMS) -> tmp
+  values(getDataTrack(mrms,i,type = c("scoreA", "scoreMean"))$scoreRMS) -> tmp
   for(j in 1:width(ranges(ranges(mrms))[[i]])){
     if(tmp[,j]>0){
       cat(paste(j,"\t",tmp[,j],"\n",sep=""))
@@ -24,10 +24,10 @@ for(i in 1:length(ranges(mrms))){
   }
 }
 sink()
-sink(paste(args[1],"Mean","wig",sep="."))
+sink(paste(args[1],"ScoreMean","wig",sep="."))
 for(i in 1:length(ranges(mrms))){
   cat(paste("variableStep chrom=",as.character(seqnames(ranges(mrms)))[i],"\n",sep=""))
-  values(getDataTrack(mrms,i,type = c("scoreRMS", "scoreMean"))$scoreMean) -> tmp
+  values(getDataTrack(mrms,i,type = c("scoreA", "scoreMean"))$scoreMean) -> tmp
   for(j in 1:width(ranges(ranges(mrms))[[i]])){
     if(tmp[,j]>0){
       cat(paste(j,"\t",tmp[,j],"\n",sep=""))
